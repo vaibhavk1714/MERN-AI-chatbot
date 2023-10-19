73ccb9827bacbadabb1e995337c3beec861efe46
Vaibhav Kanthi <vaibhav.kanthi@gmail.com>
2023-10-19 16:17:49 +0530

    Backend setup

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,14 +1,10 @@
 import express from "express";
 const app = express();
-
 //middlewares
 app.use(express.json());
-
 //connections and listeners
-
-
-
 //Port listener
 app.listen(5000, () => {
-    console.log(`Server running on port 5000`)
-})
+    console.log(`Server running on port 5000`);
+});
+//# sourceMappingURL=index.js.map
