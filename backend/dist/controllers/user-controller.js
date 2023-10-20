c11fc89e3403af012e80cd0e74a6de93bd11cdf2
Vaibhav Kanthi <vaibhav.kanthi@gmail.com>
2023-10-21 02:36:44 +0530

    Implemented authorization using jwt tokens

--- backend/dist/controllers/user-controller.js
+++ backend/dist/controllers/user-controller.js
@@ -1,5 +1,7 @@
 import { hash, compare } from 'bcrypt';
 import User from "../models/User.js";
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 export const getAllUsers = async (req, res, next) => {
     //get all users from database
     try {
@@ -17,11 +19,31 @@ export const userSignup = async (req, res, next) => {
     try {
         const { name, email, password } = req.body;
         const existingUser = await User.findOne({ email });
+        //check is user with same email is already present
         if (existingUser)
             return res.status(401).json({ message: "ERROR", cause: "User already exists" });
+        //hash the password before storing to DB
         const hashedPassword = await hash(password, 10);
         const newUser = new User({ name, email, password: hashedPassword });
         await newUser.save();
+        //remove any existing cookies
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/",
+        });
+        //create token and store cookie
+        const token = createToken(existingUser._id.toString(), existingUser.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, {
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true,
+        });
         return res.status(201).json({ message: "OK", id: newUser._id.toString() });
     }
     catch (error) {
@@ -34,12 +56,31 @@ export const userLogin = async (req, res, next) => {
     try {
         const { email, password } = req.body;
         const existingUser = await User.findOne({ email });
-        if (!existingUser) {
+        //check is user is present
+        if (!existingUser)
             return res.status(200).json({ message: "ERROR", cause: "User does not exist" });
-        }
+        //validate the hashed password
         const isPasswordCorrect = await compare(password, existingUser.password);
         if (!isPasswordCorrect)
             return res.status(403).send("Incorrect password");
+        //remove any existing cookies
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/",
+        });
+        //create token and store cookie
+        const token = createToken(existingUser._id.toString(), existingUser.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, {
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true,
+        });
         return res.status(200).json({ message: "OK", id: existingUser._id.toString() });
     }
     catch (error) {
