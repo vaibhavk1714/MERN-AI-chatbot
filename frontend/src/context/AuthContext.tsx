c6f20c65f8df31ecabbb5b6f86568015e582d93f
Vaibhav Kanthi <vaibhav.kanthi@gmail.com>
2023-10-22 01:33:28 +0530

    Implemented the header and state using contextAPI

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,48 @@
+import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+
+type User = {
+    name: string;
+    email: string;
+}
+
+type UserAuth = {
+    isLoggedIn: boolean;
+    user: User | null;
+    signup: (name: string, email: string, password: string) => Promise<void>;
+    login: (email: string, password: string,) => Promise<void>;
+    logout: () => Promise<void>;
+}
+const AuthContext = createContext<UserAuth | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        //fetch if the user's cookie are valid and skip login
+    }, [])
+
+    const login = async (email: string, password: string) => {
+
+    }
+
+    const signup = async (name: string, email: string, password: string) => {
+
+    }
+
+    const logout = async () => {
+
+    }
+
+    const value = {
+        user,
+        isLoggedIn,
+        signup,
+        login,
+        logout,
+    };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+};
+
+export const useAuth = () => useContext(AuthContext);
