2733c394f968759e2977b70885486b74c9b353f1
Vaibhav Kanthi <vaibhav.kanthi@gmail.com>
2023-10-24 02:50:42 +0530

    Completed backend for chat completion using OpenAI api

---
+++ backend/dist/config/openai-config.js
@@ -0,0 +1,9 @@
+import { Configuration } from 'openai';
+export const configureOpenAI = () => {
+    const config = new Configuration({
+        apiKey: process.env.OPEN_AI_API_KEY,
+        organization: process.env.OPEN_AI_ORGANIZATION,
+    });
+    return config;
+};
+//# sourceMappingURL=openai-config.js.map
